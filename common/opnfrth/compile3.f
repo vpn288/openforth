a1315a7d10d327198f10c58700fa73c67cee3706
vpn288 <vpn@vindenikole.ru>
2016-11-26 09:14:31 +0300

    Create compile3.f

---
+++ common/opnfrth/compile3.f
@@ -0,0 +1,28 @@
+
+  FORTH32 CONTEXT ! FORTH32 CURRENT !
+  
+   
+  WORD: (   )   WORD         ;WORD
+  WORD: NAME>   N>LINK CELL+ ;WORD
+ 
+  WORD: NONAME:   interpret# ,  immediator ;WORD 
+ 
+  WORD: XT:  HEADER COMPILE [ ' 0 @ , ]  HERE CELL+ ,  NONAME:  ;WORD 
+  
+  
+  
+  WORD: nopsing    hex, ffffffff  AND  hex, 9090909000000000 + ;WORD 
+  
+  ASSEMBLER FORTH32 LINK   ASSEMBLER CONTEXT !  ASSEMBLER CURRENT !  
+
+  WORD: forward>     HERE  0 ,   HERE      ;WORD 
+  WORD: >forward     HERE  hex, 4 + SWAP- nopsing SWAP!     ;WORD 
+  WORD: backward<    HERE                    ;WORD 
+  WORD: <backward    HERE  hex, 4 + -  nopsing ,       ;WORD 
+  
+  FORTH32 CONTEXT ! FORTH32 CURRENT !
+  
+ 
+
+  EXIT
+  
