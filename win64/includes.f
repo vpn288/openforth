90bcf858b623275773086841310dac3af5dde048
vpn288 <vpn@vindenikole.ru>
2016-12-03 09:41:22 +0300

    Create includes.f

---
+++ win64/includes.f
@@ -0,0 +1,33 @@
+    
+
+  	 INCLUDE: ../common/opnfrth/assembler64/opcodes1.f   
+  	 INCLUDE: ../common/opnfrth/assembler64/opcodes2.f
+	 INCLUDE: ../common/opnfrth/assembler64/opcodes345.f
+
+	 INCLUDE: ../common/opnfrth/lowlevel64/lowlevels1.f 
+	 INCLUDE: ../common/opnfrth/lowlevel64/compile1.f 
+	 INCLUDE: ../common/opnfrth/lowlevel64/lowlevels2.f 
+
+	 INCLUDE: ../common/opnfrth/lowlevel64/calculations.f
+	 
+	
+	 INCLUDE: ../common/opnfrth/compile2.f  
+	 INCLUDE: ../common/opnfrth/immediates/init_immediates.f  
+     INCLUDE: ../common/opnfrth/controls.f  
+	 
+	 INCLUDE: ../common/opnfrth/immediates/immediates.f   
+	 INCLUDE: ../common/opnfrth/compile3.f   
+	 INCLUDE: ../common/opnfrth/lowlevel64/numbers.f
+	 
+	 
+	 INCLUDE:  osapi.f  
+	 INCLUDE:  winapi.f  
+	 
+	 INCLUDE:  init_console.f  
+	 INCLUDE:  type.f 
+	 INCLUDE: ../common/opnfrth/numbers.f 
+	 
+	 INCLUDE:  f-system.f
+	 
+	 EXIT 
+	
