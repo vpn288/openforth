f37a11ed1898f2ec0cc082433aa466a48fa3ae84
vpn288 <vpn@vindenikole.ru>
2016-12-14 09:02:24 +0300

    Create includes.f

---
+++ kolibry/includes.f
@@ -0,0 +1,16 @@
+      INCLUDE: assembler/opcodes1.f
+      INCLUDE: assembler/opcodes2.f
+      INCLUDE: assembler/opcodes345.f
+
+	  INCLUDE: lowlevel/calculations.f
+	  INCLUDE: lowlevel/lowlevels.f
+	  
+	  INCLUDE: compile1.f
+	  INCLUDE: immediates/init_immediates.f
+      INCLUDE: controls.f
+	  INCLUDE: immediates/immediates.f
+	  INCLUDE: compile.f
+	  
+	  INCLUDE: lowlevel/calculations2.f
+
+      EXIT
