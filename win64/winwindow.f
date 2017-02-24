fc9ba5e38c50fb6f721548b8eacdc8b08045d3af
vpn288 <vpn@vindenikole.ru>
2017-02-24 08:28:53 +0300

    Update winwindow.f

--- win64/winwindow.f
+++ win64/winwindow.f
@@ -1,4 +1,71 @@
+WINAPIS:
 
+	 
+     LIB: User32.dll
+		 c_ints CreateWindowExA
+		 1_int  RegisterClassExA
+		 1_int  TranslateMessage
+		 1_int  DispatchMessageA
+		 1_int  GetDC 
+		 1_int  UpdateWindow
+		 2_ints UnregisterClassA
+		 2_ints LoadIconA
+		 2_ints LoadCursorA
+		 3_ints InvalidateRect 
+		
+		 4_ints DefWindowProcA
+		 4_ints MessageBoxA 
+		 4_ints GetMessageA
+	 
+	 LIB: Kernel32.dll
+	      1_int GetModuleHandleA
+;WINAPIS
+
+
+   
+VECT  inWinProc   
+
+VARIABLE hwnd VARIABLE wmsg VARIABLE wparam VARIABLE lparam  
+
+
+ASSEMBLER FORTH32 LINK   ASSEMBLER CONTEXT !
+
+HEADER winproc HERE CELL+ ,
+   push_rcx push_rdx push_r8 push_r9 push_rbx push_rsi push_rdi 
+  mov_rax,# hwnd ,
+ mov_[rax],rcx   
+  mov_rax,# wmsg ,
+ mov_[rax],rdx   
+  mov_rax,# wparam ,
+ mov_[rax],r8	 
+  mov_rax,# lparam ,
+ mov_[rax],r9	 
+ 
+  mov_rax,# ' inWinProc  , 
+  mov_r11,# ' Push @ ,  call_r11 
+ mov_r11,# ' EXECUTE @ , call_r11 
+ mov_r11,# ' Pop @ ,  call_r11 
+ test_rax,rax
+ jne	forward> 
+ pop_rdi pop_rsi pop_rbx pop_r9 pop_r8 pop_rdx pop_rcx
+ ret 
+ 
+  >forward  
+ pop_rdi pop_rsi pop_rbx pop_r9 pop_r8 pop_rdx pop_rcx
+push_rcx     push_rdx push_r8 push_r9 push_rbx push_rsi push_rdi 
+	 
+ mov_r11,# ' DefWindowProcA CELL+ @ ,
+sub_rsp,b# 0x 20 B,
+ call_r11 
+add_rsp,b# 0x 20 B, 
+ pop_rdi pop_rsi pop_rbx pop_r9 pop_r8 pop_rdx pop_rcx
+ret
+ALIGN
+
+FORTH32 CONTEXT !
+
+
+CREATE msg  0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,
 
 WORD: innerloop   0 Begin Pop msg hwnd @ 0 0  GetMessageA DUP  Until  ;WORD 
 
@@ -29,7 +96,7 @@ CRLF .( creating window )
 .(  ww1 file is here ) 
 
 WORD: opwn 
-   0 _class title [ CONTEXT @ defines CONTEXT ! ]  WS_VISIBLE WS_DLGFRAME WS_SYSMENU [ CONTEXT ! ] + +    0 0 hex, 150 hex, 100  0 0 hInstance 0   CreateWindowExA ."  Hwnd:" DUP hwnd !   hwnd @ GetDC hex, ffffffff AND DUP hdc !    hdc @ mybrush SelectObject Pop   
+   0 _class title [ CONTEXT @ defines CONTEXT ! ]  WS_VISIBLE WS_DLGFRAME WS_SYSMENU [ CONTEXT ! ] + +    0 0 hex, 150 hex, 100  0 0 hInstance 0   CreateWindowExA ."  Hwnd:" DUP hwnd !   hwnd @ GetDC hex, ffffffff AND DUP hdc !     
    
 ;WORD 
 
