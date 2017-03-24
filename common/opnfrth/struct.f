73a630a4e3062ed8aae02f488ff9c1f4cee3e1ce
vpn288 <vpn@vindenikole.ru>
2017-03-24 10:25:03 +0300

    Update struct.f

--- common/opnfrth/struct.f
+++ common/opnfrth/struct.f
@@ -1,40 +1,22 @@
 WORD: QWORD  VARIABLE    ['] @   ,  ['] !  ,  ;WORD 
+WORD: sp_word   HERE DUP HEADER  hex, 20202003 SWAP!  CELL+ (( name field )
+                 0 SWAP!  (( Link field )
+				 ['] NOOP @  ;WORD 
 
 
 WORD: get   (( elem_addr -- elem_value )   DUP CELL+  @ EXECUTE ;WORD 
 WORD: store (( new_value elem_addr --  )   DUP CELL+ CELL+  @ EXECUTE ;WORD 
 
-WORD:  STRUC:  CURRENT @  VARIABLE   LATEST N>LINK CELL+ CELL+  DUP CURRENT !  DOES> DUP h.  ;WORD 
-WORD:  ;STRUC  LATEST SWAP!  CURRENT !   ;WORD 
+WORD: next_elem     N>LINK @ DUP h. ;WORD 
+WORD: elem_name		DUP TYPE ;WORD 
 
-STRUC: strucname 
+WORD: get_st  ." get_struct " @ 1 hex, 4 Do  elem_name DUP NAME> CELL+ get h. next_elem Loop elem_name  ;WORD 
 
-QWORD abc  
-QWORD bde
-QWORD tyu 
+VARIABLE tmp 
+WORD: get_struct  ." get_str " tmp !  @ Begin  elem_name DUP NAME> CELL+ get h. next_elem tmp @ <> UNTIL ;WORD 
 
-QWORD edf
-;STRUC 
-
-
- strucname FORTH32 LINK  
-strucname h.  strucname @ DUP TYPE SPACE N>LINK @ DUP TYPE SPACE  N>LINK @ DUP TYPE SPACE  N>LINK  @ DUP  TYPE SPACE  N>LINK @ h. 
-
-CRLF
-.(    sdlfk )
-strucname  CONTEXT !  abc h.  abc get h.  0x 3456 abc store   abc get h. 
-.( wew )
-.( abc .( wkl;j ) .( h.   abc get h.   0x 1234 abc store     abc get h. )
-
-  WORD: QWORD  VARIABLE    ['] @   ,  ['] !  ,  ;WORD 
-
-
-WORD: get   (( elem_addr -- elem_value )   DUP CELL+  @ EXECUTE ;WORD 
-WORD: store (( new_value elem_addr --  )   DUP CELL+ CELL+  @ EXECUTE ;WORD 
-
-WORD: use   DUP   FORTH32 LINK   CONTEXT ! ;WORD 
-WORD:  STRUC:  CURRENT @  CREATE 0 ,    LATEST N>LINK CELL+ CELL+    DOES>   use ;WORD 
-WORD:  ;STRUC  LATEST SWAP!  CURRENT !   ;WORD 
+WORD:  STRUC:  CURRENT @  VARIABLE ['] get_struct ,  ['] NOOP ,  LATEST N>LINK CELL+ CELL+    DOES>  DUP DUP  FORTH32 SWAP LINK   CONTEXT !  ;WORD 
+WORD:  ;STRUC  DUP h. LATEST SWAP!   CURRENT !   ;WORD 
 
 STRUC: strucname 
 
@@ -50,20 +32,8 @@ QWORD edf
 strucname   abc h.  abc get DUP h.  0x 3456 + abc store   abc get h. 
 .( wew )
 
-
+strucname get 
   
-
-EXIT
-
-STRUC: strucname 
- fistname qword
- scndname dword
- 3rdname  byte
-;STRUC
-
-Слово STRUC: создает словарь strucname в котором создаются слова fistname scndname 3rdname и для них отводится соответственно места. 
-
-strucname scndname @    3rdname @ +  fistname ! 
 
 EXIT
 
