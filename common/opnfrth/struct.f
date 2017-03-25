31ca236f33ffc5ed7fb13e67bca8519d0a9bf26e
vpn288 <vpn@vindenikole.ru>
2017-03-25 15:38:33 +0300

    Update struct.f

--- common/opnfrth/struct.f
+++ common/opnfrth/struct.f
@@ -7,15 +7,14 @@ WORD: sp_word   HERE DUP HEADER  hex, 20202003 SWAP!  CELL+ (( name field )
 WORD: get   (( elem_addr -- elem_value )   DUP CELL+  @ EXECUTE ;WORD 
 WORD: store (( new_value elem_addr --  )   DUP CELL+ CELL+  @ EXECUTE ;WORD 
 
-WORD: next_elem     N>LINK @ DUP h. ;WORD 
+WORD: next_elem     N>LINK @ ;WORD 
 WORD: elem_name		DUP TYPE ;WORD 
+ 
 
-WORD: get_st  ." get_struct " @ 1 hex, 4 Do  elem_name DUP NAME> CELL+ get h. next_elem Loop elem_name  ;WORD 
-
-VARIABLE tmp 
-WORD: get_struct  ." get_str " tmp !  @ Begin  elem_name DUP NAME> CELL+ get h. next_elem tmp @ <> UNTIL ;WORD 
+WORD: get_struct  ." get_str " CRLF >R @ Begin  elem_name DUP NAME> CELL+ get h. CRLF  next_elem DUP R@ < NOT Until RDROP ;WORD 
 
 WORD:  STRUC:  CURRENT @  VARIABLE ['] get_struct ,  ['] NOOP ,  LATEST N>LINK CELL+ CELL+    DOES>  DUP DUP  FORTH32 SWAP LINK   CONTEXT !  ;WORD 
+
 WORD:  ;STRUC  DUP h. LATEST SWAP!   CURRENT !   ;WORD 
 
 STRUC: strucname 
