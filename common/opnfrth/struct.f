2e5ea2bff7ad948d402668585999264d8ca1022b
vpn288 <vpn@vindenikole.ru>
2017-03-27 08:42:17 +0300

    Update struct.f

--- common/opnfrth/struct.f
+++ common/opnfrth/struct.f
@@ -1,21 +1,24 @@
 WORD: QWORD  VARIABLE    ['] @   ,  ['] !  ,  ;WORD 
-WORD: sp_word   HERE DUP HEADER  hex, 20202003 SWAP!  CELL+ (( name field )
-                 0 SWAP!  (( Link field )
-				 ['] NOOP @  ;WORD 
-
 
 WORD: get   (( elem_addr -- elem_value )   DUP CELL+  @ EXECUTE ;WORD 
 WORD: store (( new_value elem_addr --  )   DUP CELL+ CELL+  @ EXECUTE ;WORD 
 
-WORD: next_elem     N>LINK @ ;WORD 
-WORD: elem_name		DUP TYPE ;WORD 
+WORD: next_elem     N>LINK DUP h.  @ ;WORD 
+WORD: elem_name		DUP TYPE SPACE ;WORD 
  
+WORD: get_struct  
+         DUP  >R  @ Begin  DUP NAME> CELL+ get SWAP  next_elem DUP R@ < NOT Until RDROP ;WORD 
 
-WORD: get_struct  ." get_str " CRLF >R @ Begin  elem_name DUP NAME> CELL+ get h. CRLF  next_elem DUP R@ < NOT Until RDROP ;WORD 
+WORD:  STRUC:  
+          HERE
+		  VARIABLE ['] 
+		  get_struct ,  ['] NOOP ,  
+		  LATEST NAME> CELL+ (( N>LINK CELL+ CELL+   )
+		  make_badword
+		  DOES>  DUP DUP  FORTH32  LINK   CONTEXT !  ;WORD 
 
-WORD:  STRUC:  CURRENT @  VARIABLE ['] get_struct ,  ['] NOOP ,  LATEST N>LINK CELL+ CELL+    DOES>  DUP DUP  FORTH32 SWAP LINK   CONTEXT !  ;WORD 
+WORD:  ;STRUC   LATEST SWAP!   CURRENT @ !   ;WORD 
 
-WORD:  ;STRUC  DUP h. LATEST SWAP!   CURRENT !   ;WORD 
 
 STRUC: strucname 
 
@@ -26,15 +29,46 @@ QWORD tyu
 QWORD edf
 ;STRUC 
 
-
 .(    sdlfk )
-strucname   abc h.  abc get DUP h.  0x 3456 + abc store   abc get h. 
+  strucname h.     abc h.  abc get DUP h.  0x 3456 + abc store   abc get h.  abc get 0x 9 + bde  store  0x 9 tyu  store 0x 4 edf store  FORTH32 CONTEXT !  strucname UNLINK 
 .( wew )
 
-strucname get 
+CURRENT @ @ TYPE 
+
+
+ WORD: nhj    [ strucname  abc LIT, ]  get   h. ;WORD  
+
+LATEST TYPE 
+ nhj FORTH32 CONTEXT !  strucname UNLINK 
+ 
+ WORD: wordlist   0 hex, 13 Do elem_name next_elem Loop   ;WORD 
+
+ 
+EXIT
+
+
+
+
+
+
+strucname get  h. h. h. h. h. 
+
+strucname UNLINK 
   
 
-EXIT
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 STRUC: strucname 
  fistname qword
