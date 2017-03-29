036e7aa6b131f70e1bf1ac920f59d99e7b14743b
vpn288 <vpn@vindenikole.ru>
2017-03-29 15:11:09 +0300

    Update struct.f

--- common/opnfrth/struct.f
+++ common/opnfrth/struct.f
@@ -6,16 +6,21 @@ WORD: store (( new_value elem_addr --  )   DUP CELL+ CELL+  @ EXECUTE ;WORD
 WORD: next_elem     N>LINK DUP h.  @ DUP h. CRLF ;WORD 
 WORD: elem_name		DUP TYPE SPACE ;WORD 
  
-WORD: get_struct  ."  getting_struct "
-         DUP  >R  @ Begin  DUP NAME> CELL+ get SWAP  next_elem elem_name DUP R@ < NOT Until RDROP ;WORD 
+WORD: get_struct  
+            Begin @ DUP TYPE N>LINK DUP CELL+ CELL+ get SWAP  Again  ;WORD 
+			
+WORD: on_bad   RDROP RDROP RDROP RDROP RDROP   ;WORD 
 		 
 WORD: (struct) CREATE HERE  0 , ['] get_struct ,  ['] NOOP , DOES>   DUP CONTEXT !  ;WORD 
+
+WORD: nxt  CRLF @ DUP TYPE N>LINK DUP CELL+ CELL+ ;WORD 
 		 
 WORD:  STRUC:  
           CURRENT @ 
 		  (struct)   DUP  
 		   
 		  HERE SWAP! make_badword  
+		  ['] on_bad ,  ['] NOOP , 
 		  DUP CURRENT ! DUP CONTEXT @ LINK   CONTEXT ! 
 		  
 		    ;WORD 
@@ -31,9 +36,18 @@ QWORD edf
 ;STRUC  .(  closetrucname )  
 
  strucname @ elem_name next_elem  elem_name next_elem  elem_name next_elem  elem_name next_elem  elem_name  next_elem h. 
+ 
+CRLF .(  manual_get_struct ) CRLF 
 
-strucname   0x ead tyu store  tyu  get h. CRLF 
-strucname get  h. h. h. h. h. 
+
+
+ strucname  nxt get h.   nxt get h. nxt get h.  nxt get h.     
+ 
+ 
+CRLF .(  store-get_test )
+strucname   0x ead tyu store  tyu  get h.  0x 4 edf get + edf store   CRLF 
+
+ strucname get  h. h. h. h. h. .( jkhh )
 
  
 EXIT
