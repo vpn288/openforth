25358ff9ae195ff392021c4c743dcbe9cf788e04
vpn288 <vpn@vindenikole.ru>
2017-03-29 08:59:57 +0300

    Update struct.f

--- common/opnfrth/struct.f
+++ common/opnfrth/struct.f
@@ -3,52 +3,46 @@ WORD: QWORD  VARIABLE    ['] @   ,  ['] !  ,  ;WORD
 WORD: get   (( elem_addr -- elem_value )   DUP CELL+  @ EXECUTE ;WORD 
 WORD: store (( new_value elem_addr --  )   DUP CELL+ CELL+  @ EXECUTE ;WORD 
 
-WORD: next_elem     N>LINK DUP h.  @ ;WORD 
+WORD: next_elem     N>LINK DUP h.  @ DUP h. CRLF ;WORD 
 WORD: elem_name		DUP TYPE SPACE ;WORD 
  
-WORD: get_struct  
-         DUP  >R  @ Begin  DUP NAME> CELL+ get SWAP  next_elem DUP R@ < NOT Until RDROP ;WORD 
-
+WORD: get_struct  ."  getting_struct "
+         DUP  >R  @ Begin  DUP NAME> CELL+ get SWAP  next_elem elem_name DUP R@ < NOT Until RDROP ;WORD 
+		 
+WORD: (struct) CREATE HERE  0 , ['] get_struct ,  ['] NOOP , DOES>   DUP CONTEXT !  ;WORD 
+		 
 WORD:  STRUC:  
-          HERE
-		  VARIABLE ['] 
-		  get_struct ,  ['] NOOP ,  
-		  LATEST NAME> CELL+ (( N>LINK CELL+ CELL+   )
-		  make_badword
-		  DOES>  DUP DUP  FORTH32  LINK   CONTEXT !  ;WORD 
+          CURRENT @ 
+		  (struct)   DUP  
+		   
+		  HERE SWAP! make_badword  
+		  DUP CURRENT ! DUP CONTEXT @ LINK   CONTEXT ! 
+		  
+		    ;WORD 
 
-WORD:  ;STRUC   LATEST SWAP!   CURRENT @ !   ;WORD 
+WORD:  ;STRUC      CURRENT  !   ;WORD 
 
 
-STRUC: strucname 
-
+STRUC: strucname    
 QWORD abc  
-QWORD bde
+QWORD bde   
 QWORD tyu 
+QWORD edf  
+;STRUC  .(  closetrucname )  
 
-QWORD edf
-;STRUC 
+ strucname @ elem_name next_elem  elem_name next_elem  elem_name next_elem  elem_name next_elem  elem_name  next_elem h. 
 
-.(    sdlfk )
-  strucname h.     abc h.  abc get DUP h.  0x 3456 + abc store   abc get h.  abc get 0x 9 + bde  store  0x 9 tyu  store 0x 4 edf store  FORTH32 CONTEXT !  strucname UNLINK 
-.( wew )
-
-CURRENT @ @ TYPE 
-
-
- WORD: nhj    [ strucname  abc LIT, ]  get   h. ;WORD  
-
-LATEST TYPE 
- nhj FORTH32 CONTEXT !  strucname UNLINK 
- 
- WORD: wordlist   0 hex, 13 Do elem_name next_elem Loop   ;WORD 
+strucname   0x ead tyu store  tyu  get h. CRLF 
+strucname get  h. h. h. h. h. 
 
  
 EXIT
 
+Header записывает в поле связи значение, полученное по latest. a here записывает в CURRENT @ 
+HERE N>LINK LATEST SWAP!   HERE CURRENT @ ! 
 
 
-
+ strucname @ elem_name next_elem  elem_name next_elem  elem_name next_elem  elem_name next_elem  elem_name  next_elem elem_name
 
 
 strucname get  h. h. h. h. h. 
