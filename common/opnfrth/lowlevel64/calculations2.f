ef4b036b7e217c35f68bba23a7e695bfdc1d6382
vpn288 <vpn@vindenikole.ru>
2017-04-08 16:39:48 +0300

    Create calculations2.f

---
+++ common/opnfrth/lowlevel64/calculations2.f
@@ -0,0 +1,46 @@
+  
+
+ASSEMBLER FORTH32 LINK    ASSEMBLER CONTEXT !	  FORTH32 CURRENT !
+
+ HEADER w(swap_ab) HERE CELL+ , 
+  mov_rdx,# ' Pop @ , call_rdx 
+  rol_rax,# 0x 10 B,  
+  mov_rdx,# ' Push @ , call_rdx 
+  ret 
+  ALIGN
+  
+  HEADER ?MAX HERE CELL+ ,
+  mov_rdx,# ' Pop @ ,   call_rdx              
+  xor_rcx,rcx
+  mov_rcx,rax           
+  call_rdx     
+  cmp_rax,rcx
+  cmovc_rax,rcx
+  setc_cl
+  mov_rdx,#  ' Push @ ,   call_rdx           
+  mov_rax,rcx
+  call_rdx
+  ret
+  ALIGN
+  
+  
+  CREATE a_arg  0 , 0 , 0 , 0 ,
+  CREATE b_arg  0 , 0 , 0 , 0 ,
+  
+  ASSEMBLER FORTH32 LINK  
+  
+  HEADER SADD  HERE CELL+ ,
+   mov_rax,# a_arg ,
+   movdqu_xmm1,[rax]
+   mov_rax,#   b_arg , 
+   movdqu_xmm2,[rax]  
+   psubd_xmm2,xmm1
+   pabsd_xmm1,xmm2
+   phaddd_xmm1,xmm2 
+   mov_rax,# a_arg , 
+   movdqu_[rax],xmm2
+  ret
+  ALIGN
+  
+  
+  EXIT  
