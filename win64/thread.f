b9ba0c074deb7a44a9fd57584e1b970a6f8a068b
vpn288 <vpn@vindenikole.ru>
2017-05-01 09:27:40 +0300

    Update thread.f

--- win64/thread.f
+++ win64/thread.f
@@ -1,25 +1,115 @@
 WINAPIS:
    LIB: Kernel32.dll
-       5_ints CreateThread
+       6_ints CreateThread
 	   4_ints VirtualAlloc 
-	   void   GetCurrentThread
+	   3_ints OpenThread 
+	   1_int  ExitThread
+	   1_int  ResumeThread
+	   1_int  SuspendThread
+	   void   GetCurrentThreadId
 ;WINAPIS 
-
-
  
- GetCurrentThread .( main thread ) h. 
+WORD: get_thread_handle 
+                 GetCurrentThreadId 
+;WORD 
 
+CREATE threads 0 , 0 , 0 , 0 , 0 , 0 ,
+CREATE mems    0 , 0 , 0 , 0 , 0 , 0 , 
+VARIABLE n_th   
+
+get_thread_handle h. 
 	
 VECT ThreadProc
 
 VARIABLE thr 
 VARIABLE thr_stack
+VARIABLE thread_id
+
+				 
+ 
+WORD: starttr:  >R 0 0 R>  DUP  ."  thread_low:"  h. 
+                 0  hex, 10000   hex, 3000  hex, 40    VirtualAlloc 
+ 				 hex, 4  0   CreateThread    ;WORD 
+ 
+ASSEMBLER FORTH32 LINK ASSEMBLER CONTEXT ! IMMEDIATES CURRENT ! 
+
+WORD:  thread_low 
+    LATEST NAME>  HERE >R
+	mov_rax,rcx
+    mov_rbp,rsp
+    sub_rsp,b# hex, 70 B, 
+
+    mov_[rbp+b#],rax 0 B,
+    xor_rax,rax
+    mov_[rbp+b#],rax hex, F8 B,
+	mov_rax,rcx
+	mov_r11,#  COMPILE [ ' Push @ , ]    call_r11 
+	mov_rax,#   , 
+    mov_r11,#  COMPILE [ ' Push @ , ]    call_r11 
+    mov_r11,#  COMPILE [ ' EXECUTE @ , ]   call_r11
+   (( thread finish )
+ 
+    mov_r11,# COMPILE [ ' ExitThread , ] call_r11
+ALIGN  R>
+ ;WORD 
+
+
+FORTH32 CONTEXT ! FORTH32 CURRENT ! 
+
+WORD: tread+ 
+                  
+				  n_th @ mems + !  
+				  get_thread_handle n_th @ threads + !
+                  n_th @ CELL+ n_th !  				  ;WORD 
+				  
+ thread+ 
+				  
+WORD: thread>mem 
+                  GetCurrentThreadId 
+				  0 n_th @ Do DUP  threads R@ CELLs + @ <> If Pop  R@ CELLs mems + @ RDROP n_th @ >R   Then   Loop ;WORD 				  
+
+WORD: inthread1     
+                  tread+
+				  Begin thr @ 1+ DUP thr !  h. ." thred 2 "   thread>mem  h.   hex, 1700 Sleep Pop Again    thread_low  ;WORD
+ 
+starttr: CONSTANT mytr1
+
+
+WORD: inthread2  
+                 tread+
+                 Begin thr @ 1+ DUP thr !  h. ." thred 3 " thread>mem   h. hex, 1937 Sleep Pop Again      thread_low   ;WORD
+ 
+starttr: CONSTANT mytr2
+
+ 
+EXIT 
+
+starttr: CONSTANT mytr 
+
+ WORD: inthread     Begin thr @ 1+ DUP thr !  h. ." thred 2 " GetCurrentThreadId  h.    hex, 400 Sleep Pop Again ;WORD 
+
+   ' inthread TO ThreadProc   
+ WORD: alloc_mem  0  hex, 10000   hex, 3000  hex, 40    VirtualAlloc ;WORD 
+
+ DUP .( virt_alloc:) h. thr_stack ! 
+ 
+  0 0   ' thread_low @                    thr_stack @ 
+ 				 0x 4   thread_id CreateThread  
+
+
+hex, 10000   hex, 3000  hex, 40    VirtualAlloc DUP h.
+
+				  
+  
+				
+				  
+ starttr
+
+
+
 VARIABLE RSP 
 WORD: stack_base_address   ['] Pop CELL- CELL- CELL- CELL- ;WORD 
 
-0 0x 10000 0x 3000  0x 40  VirtualAlloc thr_stack !  thr_stack @ h. 
-
-ASSEMBLER FORTH32 LINK ASSEMBLER CONTEXT !
 ASSEMBLER CURRENT !
 0x 22 0x 89 0x 48 0x 3 opcode mov_[rdx],rsp 
 
@@ -31,15 +121,7 @@ HEADER rsp@  HERE CELL+ ,
 ret
 ALIGN 
 
-HEADER  thread_test  HERE CELL+ , 
-    mov_rax,rcx
-    mov_rbp,rsp
-   sub_rsp,b# 0x 70 B, 
-    mov_[rbp+b#],rax 0 B,
-   xor_rax,rax
-   mov_[rbp+b#],rax 0x F8 B,
-   
-   HERE
+ HERE
   mov_rax,# ' ThreadProc  , 
   mov_r11,# ' Push @ ,    call_r11 
   mov_r11,# ' EXECUTE @ , call_r11 
@@ -47,22 +129,13 @@ HEADER  thread_test  HERE CELL+ ,
  mov_r11,#  , 
   jmp_r11   
   
- 
-ALIGN
+  
+( HEADER abcd  ' thread_start @ ,  HERE SWAP! interpret# ,  ' inthread , ' EXIT , )
 
 WORD: Thread:  CREATE  
-               0 0  (( здесь должен быть адрес начала треда )
+               0 0  ['] thread_test @ 
                0 hex, 12000 hex, 3000 hex, 40  VirtualAlloc  
-			   hex, 4 CreateThread 
+			   hex, 4 CreateThread  , DOES> @ 
 ;WORD 
 
 FORTH32 CONTEXT !
-
-WORD: tred   thr @ 1+ DUP thr !  h.  ." thred 2 "  hex, 40 Sleep Pop ;WORD 
-
-' tred   ' ThreadProc CELL+ ! 
-
-
- 0 0 ' thread_test @  thr_stack @  0 CreateThread h. 
-
-EXIT 
